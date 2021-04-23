914da973fcd52b188d360f6091382b4a6a9b9788
Chris West (Faux) <git@goeswhere.com>
2021-04-23 08:00:23 +0100

    feat: additionally mock require-in-the-middle

--- src/fun-runtime.js
+++ src/fun-runtime.js
@@ -1,20 +1,67 @@
 const { default: JestRuntime } = require("jest-runtime");
-const ourtel = require("../app/otel");
+const otel = require('@opentelemetry/api');
 
 module.exports = class FunRuntime extends JestRuntime {
+  // ([
+  //   modules: string[] | undefined,
+  //   options: { internals?: true },
+  //   onRequire: (
+  //     exports: realModule,
+  //     name: string,
+  //     basedir: string | undefined,
+  //   ) => realModule,
+  // ] | undefined)[]
+  ritms = [];
+
   constructor() {
     super(...arguments);
-    this._environment.global.REAL_OTEL_API = require('@opentelemetry/api');
+    this._environment.global.REAL_OTEL_API = otel;
+
+    this._environment.global.MOCK_RITM = (modulesOrOptionsOrFunction, optionsOrFunction, onRequire) => {
+      // https://www.npmjs.com/package/require-in-the-middle
+      this.ritms.push(parseArgs(modulesOrOptionsOrFunction, optionsOrFunction, onRequire));
+      const id = this.ritms.length - 1;
+      return { unhook: () => this.ritms[id] = undefined };
+    }
   }
 
   _requireCoreModule(moduleName /* string */) {
     let upper = super._requireCoreModule(moduleName);
-    if (!["http", "https"].includes(moduleName)) return upper;
-    for (const ins of ourtel.instrumentations) {
-      for (const module of ins._modules) {
-        upper = ins._onRequire(module, upper, moduleName, null);
-      }
+    for (const ritm of this.ritms) {
+      if (!ritm) continue;
+      const [modules, options, onRequire] = ritm;
+      if (!modules.includes(moduleName)) continue;
+      upper = onRequire(upper, moduleName, undefined);
     }
     return upper;
   }
 };
+
+// -ing js developers
+function parseArgs(modulesOrOptionsOrFunction, optionsOrFunction, onRequire) {
+  let modules, options, func;
+  if (onRequire) {
+    modules = modulesOrOptionsOrFunction;
+    options = optionsOrFunction;
+    func = onRequire;
+  } else if (optionsOrFunction) {
+    if (Array.isArray(modulesOrOptionsOrFunction)) {
+      modules = modulesOrOptionsOrFunction;
+      options = {};
+      func = optionsOrFunction;
+    } else {
+      modules = undefined;
+      options = modulesOrOptionsOrFunction;
+      func = optionsOrFunction;
+    }
+  } else {
+    modules = undefined;
+    options = {};
+    func = modulesOrOptionsOrFunction;
+  }
+
+  if (typeof options !== 'object') throw new Error(`error parsing insane arguments for options: ${typeof options}`);
+  if (typeof func !== 'function') throw new Error(`error parsing insane arguments for func: ${typeof func}`);
+  if (modules && !Array.isArray(modules)) throw new Error(`error parsing insane arguments for modules: ${modules}`);
+  return [ modules, options, func ];
+}
--- test/needle.test.js
+++ test/needle.test.js
@@ -1,12 +1,13 @@
 jest.mock('@opentelemetry/api', () => global.REAL_OTEL_API);
+jest.mock('require-in-the-middle', () => global.MOCK_RITM);
+const { start, stop, trace } = require("../app/otel");
+start();
 
 const otel = require("@opentelemetry/api");
 otel.diag.setLogger(new otel.DiagConsoleLogger(), otel.DiagLogLevel.DEBUG);
 const needle = require("needle");
-const { start, stop, trace } = require("../app/otel");
 
 describe("foo", () => {
-  beforeAll(start);
   it("fetches", async () => {
     await trace("fetch test", async () => {
       expect(await needle("get", "https://b.fau.xxx/404")).toMatchObject({
